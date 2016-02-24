c0d57340cb9a245132a2666b699353003023018b
caedes <laurentromain@gmail.com>
2016-02-24 22:15:46 +0100

    ADD random delay

--- bot.rb
+++ bot.rb
@@ -10,10 +10,16 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
+def delay
+  min = (ENV['TWITTER_DELAY'].to_i * 0.50).to_i
+  max = (ENV['TWITTER_DELAY'].to_i * 1.05).to_i
+  rand(min..max)
+end
+
 loop do
   emotee = Emotee.generate
   client.update emotee
   puts "Emotee tweeted: #{emotee}"
 
-  sleep ENV['TWITTER_DELAY'].to_i
+  sleep delay
 end
